d91c187b04e6df6b27340458e5a801014ca8a893
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-06-23 13:00:16 -0700

    Building letter input

--- main.js/script.js
+++ main.js/script.js
@@ -7,16 +7,32 @@ const errorMax = 6;
   /*----- state variables -----*/
 let word = wordBank[Math.floor(Math.random() * wordBank.length)];
 let remainingLetters = word.length;
-let answerArr = [];
 let errors = 0;
-let inputLetter = prompt("Guess a letter: ")
+let answerArr = [];
+//for (let i = 0; i < word.length //....resume here//)
 
 
+/*to do
+turn answerArr into an array of underscores, display in a div
+accept userInput as a letter
+check array if that letter exists in the word
+if correct: slice/splice/push the correct letter into the underscore array
+if incorrect: errors++
+
+function gameOver(){
+    if (errors = errorMax){
+        innerText of "message" says you lose
+    }
+}
+*/
 
   /*----- cached elements  -----*/
-  const lossMessageEl = document.getElementById("message")
-  const playAgainBtn = document.getElementById("play-again-btn")
+  let lossMessageEl = document.getElementById("message");
+  let playAgainBtn = document.getElementById("play-again-btn");
+  let submitBtn = document.getElementById("submit-btn");
+  let inputLetter = document.getElementById("input-letter").value;
   //Hangman
+  const hangmanEls = [...document.querySelectorAll("#hangman > div")]; //for iterating visibility
   const headEl = document.getElementById("head");
   const torsoEl = document.getElementById("torso");
   const leftLegEl = document.getElementById("left-leg");
@@ -26,6 +42,7 @@ let inputLetter = prompt("Guess a letter: ")
 
   /*----- event listeners -----*/
 playAgainBtn.addEventListener("click", init);
+submitBtn.addEventListener("click", getLetter);
 
   /*----- functions -----*/
 init();
@@ -44,17 +61,38 @@ function renderMessage(){
     win();
 }
 
+function getLetter(){
+    console.log(inputLetter);
+    // if (inputLetter !=""){
+    //   inputLetter = "";
+    //}
+//    if (inputLetter != null) {
+//        
+//    }
+//    while (errors != errorMax)
+
+}
+
+
+/* note: would this be better as a ternary? See Connect Four hideMarker, bottom of js:
+
+hangmanEls.forEach(function(div, idx){
+    const displayDiv = errors > 0; //???
+    div.style.visibility = displayDiv ? "hidden" : "visible";
+})
+
 function toggleHangman(){
 headEl.style.visibility = "hidden";
 //if (errors = 1){headEl.style.visibility = visible}
 torsoEl.style.visibility = "hidden";
-//if (errors = 1){torsoEl.style.visibility = visible}
+//if (errors = 2){torsoEl.style.visibility = visible}
 leftLegEl.style.visibility = "hidden";
-//if (errors = 1){leftLegEl.style.visibility = visible}
+//if (errors = 3){leftLegEl.style.visibility = visible}
 rightLegEl.style.visibility = "hidden";
-//if (errors = 1){rightLegEl.style.visibility = visible}
+//if (errors = 4){rightLegEl.style.visibility = visible}
 leftArmEl.style.visibility = "hidden";
-//if (errors = 1){leftArmEl.style.visibility = visible}
+//if (errors = 5){leftArmEl.style.visibility = visible}
 rightArmEl.style.visibility = "hidden";
-//if (errors = 1){rightArmEl.style.visibility = visible}
+//if (errors = 6){rightArmEl.style.visibility = visible}
 }
+*/
